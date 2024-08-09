c458e343d46ce2b2a3673fd5174304bb9e80f0a5
Ratan <ratanrathod870@gmail.com>
2024-08-09 19:11:03 +0530

    hero section done

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,16 @@
+import { curve } from "../assets";
+import Button from "./Button";
+import Section from "./Section";
+import Heading from "./Heading";
+
+const Benefits = () => {
+  return (
+  <Section id="features">
+    <div className="container relative z-2">
+      <Heading/>
+    </div>
+  </Section>
+  );
+};
+
+export default Benefits;
