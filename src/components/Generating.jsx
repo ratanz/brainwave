095198fe394fff8678a0bc15a0719912af1504c7
Ratan <ratanrathod870@gmail.com>
2024-08-10 16:30:43 +0530

    pricing section code under process

--- src/components/Generating.jsx
+++ src/components/Generating.jsx
@@ -4,7 +4,7 @@ const Generating = ({ className }) => {
     return (
         <div className={`flex items-center h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem] ${className || ""} text-base`}>
             <img src={loading} alt="loading" className="w-5 h-5 mr-4" />
-            AI is generating your answer...
+            AI is generating...
         </div>
     )
 }
