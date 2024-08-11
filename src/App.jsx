2f73fda81f4d47d20dbf8499365ee8cc0fb4abf1
Ratan <ratanrathod870@gmail.com>
2024-08-11 11:53:14 +0530

    added Speed Insights into the project

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Roadmap from './components/Roadmap';
 import Footer from './components/Footer';
 
 
+
 const App = () => {
   return (
     <>
@@ -24,6 +25,7 @@ const App = () => {
       </div >
         <ButtonGradient />
     </>
+    
   )
 }
 
